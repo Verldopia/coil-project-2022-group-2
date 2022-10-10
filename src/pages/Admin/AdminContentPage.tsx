f7c3d2a834c14b74c92fa49f6cf543bc5525be48
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-10-10 19:02:51 +0200

    22.41.1.8: Added content uploader page

--- src/pages/Admin/AdminContentPage.tsx
+++ src/pages/Admin/AdminContentPage.tsx
@@ -1,9 +1,83 @@
+import { Button, TextField } from '@mui/material';
 import React from 'react';
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
+import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
+import { ROUTES } from '../../constants/routes';
 
 const AdminContentPage: React.FC = () => {
   return (
     <>
-      <p>hello content</p>
+      <form className="admin-form-container" action="">
+        <div className="admin-form-box">
+          <h2 className="form-title">Content uploader</h2>
+          <div className="admin-form-box">
+            <TextField
+              className="form-input"
+              size="small"
+              variant="outlined"
+              label="Title"
+              type="text"
+            />
+          </div>
+          <div className="admin-form-box selector">
+            <select>
+              <option value="0">Select category</option>
+              <option value="1">Camera</option>
+              <option value="2">Sensor</option>
+              <option value="3">Touchpad</option>
+              <option value="4">Thermostat</option>
+            </select>
+          </div>
+          <div className="admin-form-box">
+            <TextField
+              className="form-input"
+              size="medium"
+              variant="outlined"
+              label="Description"
+              type="text"
+            />
+          </div>
+          <div className="admin-form-box--flex">
+            <div className="admin-form-box">
+              <TextField
+                className="form-input"
+                size="small"
+                variant="outlined"
+                label="Price"
+                type="number"
+              />
+            </div>
+            <div className="admin-form-box">
+              <TextField
+                className="form-input"
+                size="small"
+                variant="outlined"
+                label="Stock"
+                type="number"
+              />
+            </div>
+          </div>
+          <p>Upload images</p>
+          <div className="admin-form-box">
+            <TextField
+              className="form-input"
+              size="medium"
+              variant="outlined"
+              type="file"
+            />
+          </div>
+          <div className="form-btn">
+            <Button
+              variant="contained"
+              color="success"
+              startIcon={<VpnKeyOutlinedIcon />}
+            >
+              Upload
+            </Button>
+          </div>
+        </div>
+      </form>
     </>
   );
 };
