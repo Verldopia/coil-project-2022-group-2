704d1bd9caf491e60404a4e716c9fbfcd407e81b
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-10-19 01:06:34 +0200

    22.42.3.1: Added routing for categories

--- server/src/categories/categories.resolver.ts
+++ server/src/categories/categories.resolver.ts
@@ -1,4 +1,10 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+} from '@nestjs/graphql';
 import { CategoriesService } from './categories.service';
 import { Category } from './entities/category.entity';
 import { CreateCategoryInput } from './dto/create-category.input';
@@ -6,11 +12,14 @@ import { UpdateCategoryInput } from './dto/update-category.input';
 
 @Resolver(() => Category)
 export class CategoriesResolver {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(
+    private readonly categoriesService: CategoriesService
+  ) {}
 
   @Mutation(() => Category)
   createCategory(
-    @Args('createCategoryInput') createCategoryInput: CreateCategoryInput,
+    @Args('createCategoryInput')
+    createCategoryInput: CreateCategoryInput
   ) {
     return this.categoriesService.create(createCategoryInput);
   }
@@ -22,17 +31,28 @@ export class CategoriesResolver {
 
   // Find one by ID
   @Query(() => Category)
-  getCategory(@Args('id', { type: () => Int }) id: number): Promise<Category> {
+  getCategory(
+    @Args('id', { type: () => Int }) id: number
+  ): Promise<Category> {
     return this.categoriesService.getCategory(id);
+  }
+
+  // Find one by title
+  @Query(() => Category)
+  getCategoryTitle(
+    @Args('title', { type: () => String }) title: string
+  ): Promise<Category> {
+    return this.categoriesService.getCategoryTitle(title);
   }
 
   @Mutation(() => Category)
   updateCategory(
-    @Args('updateCategoryInput') updateCategoryInput: UpdateCategoryInput,
+    @Args('updateCategoryInput')
+    updateCategoryInput: UpdateCategoryInput
   ) {
     return this.categoriesService.update(
       updateCategoryInput.id,
-      updateCategoryInput,
+      updateCategoryInput
     );
   }
 
