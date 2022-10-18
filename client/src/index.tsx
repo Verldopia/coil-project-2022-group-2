29708dffd4e497ffed8ecb7ce6376e447e535821
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-10-18 21:18:10 +0200

    22.42.2.2: Added apollo client
    Data is now receivable in wishlist

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,25 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 
-const client = new QueryClient();
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <QueryClientProvider client={client}>
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </QueryClientProvider>
+  </ApolloProvider>
 );
