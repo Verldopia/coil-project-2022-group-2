c8d1f6db907977071c9fab314f003a4110782ebc
Michiel Willems <91065562+Verldopia@users.noreply.github.com>
2022-10-19 15:07:36 +0200

    22.42.3.3: Reverted multiple repo

--- client/src/components/.layouts/BaseLayout.tsx
+++ client/src/components/.layouts/BaseLayout.tsx
@@ -11,7 +11,6 @@ const BaseLayout: React.FC<IBaseLayoutProps> = (props) => {
       <Header />
       <div className="baseLayout">
         <Outlet />
-        {/* Yeet */}
       </div>
       <Footer />
     </>
