23e553c3d6b937bba4a2096bb1054b2082b29c7b
Michiel Willems <91065562+Verldopia@users.noreply.github.com>
2022-10-30 22:20:07 +0100

    22.43.7.6: Added "remove all" functionality

--- client/src/pages/WishlistPage.tsx
+++ client/src/pages/WishlistPage.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { ProductsData } from '../interfaces';
 import { GET_ALL_PRODUCTS } from '../graphql/products';
-import { ProductCard } from '../components';
+import { DescriptionBox, Popular, ProductCard } from '../components';
+
+// Styles
+import { Button, Tooltip } from '@mui/material';
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
 
 const WishlistPage = () => {
   // Fetch products
@@ -13,25 +17,76 @@ const WishlistPage = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Can't load wishlist.</p>;
 
-  const wishlist = JSON.parse(localStorage.wishlist);
-  console.log('🚀 - wishlist', wishlist);
+  let newList = new Array();
+  if (localStorage.wishlist) {
+    // Get data from localStorage
+    let wishlist = JSON.parse(localStorage.wishlist);
 
-  const products = data?.Items;
+    // Search where wishlistID === productID
+    wishlist.forEach((id: number) => {
+      const product = data?.Items.find((prod) => id === prod.id);
+      newList.push(product);
+    });
+  }
 
-  for (let i = 0; i < wishlist.length; i++) {
-    const product = products?.filter(
-      (prod) => prod.id === wishlist[i]
-    );
+  // Remove all wishlistItems when clicked
+  function removeAll() {
+    localStorage.removeItem('wishlist');
+    window.location.reload();
   }
 
   return (
-    <ul>
-      {data?.Items.filter((prod) => prod.id === wishlist[1]).map(
-        (item) => (
-          <ProductCard item={item} i={1} key={1} />
-        )
+    <div className="box">
+      {newList[0] && (
+        <div className="container--box">
+          {/* // Total amount of products */}
+          {newList.length && (
+            <p className="bread--box">
+              {newList.length} products found.
+            </p>
+          )}
+          <Tooltip
+            title="Remove all items from wishlist"
+            arrow
+            placement="bottom"
+          >
+            <Button
+              className="mainBtn removeBtn"
+              color="secondary"
+              variant="outlined"
+              onClick={removeAll}
+            >
+              <HeartBrokenIcon />
+              Remove all
+            </Button>
+          </Tooltip>
+        </div>
       )}
-    </ul>
+      {newList[0] ? (
+        <ul className="product-container">
+          <DescriptionBox
+            title={'Your wishlist'}
+            text={'All your favorite products, in one spot!'}
+          />
+
+          {newList.map((item, i) => (
+            <ProductCard button={true} item={item} i={i} key={i} />
+          ))}
+        </ul>
+      ) : (
+        // Displayed when there is no wishlist
+        <div className="form-container">
+          <p>
+            Your <strong className="bold">wishlist</strong> is empty.
+          </p>
+          <p>
+            You can add products to this list, to easily find them
+            later.
+          </p>
+          <Popular />
+        </div>
+      )}
+    </div>
   );
 };
 
